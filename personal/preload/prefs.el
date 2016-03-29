23dfe0de0a4c349edf27dba53e5f88eff8878403
Eric Devost <eric.devost@frimastudio.com>
2016-03-29 08:51:35 -0400

    personnal config for color theme

--- personal/preload/prefs.el
+++ personal/preload/prefs.el
@@ -1,1 +1,13 @@
-(setq prelude-theme 'solarized-dark)
+;;; package --- Summary
+;;; Commentary:
+;;; Code:
+
+;;; Color theme
+(color-theme-initialize)
+(color-theme-dark-laptop)
+
+;;; Set yaml mode with files ending in sls, for salt stack
+(add-to-list 'auto-mode-alist '("\\.sls\\'" . yaml-mode))
+
+(provide 'prefs.el)
+;;; prefs.el ends here
