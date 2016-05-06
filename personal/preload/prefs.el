b399b9c8c40a0afcaa1a3b2a8fe3abe2c9509791
Eric Devost <eric.devost@frimastudio.com>
2016-05-06 16:17:41 -0400

    new prefs for git 2.0

--- personal/preload/prefs.el
+++ personal/preload/prefs.el
@@ -3,11 +3,21 @@
 ;;; Code:
 
 ;;; Color theme
-(color-theme-initialize)
-(color-theme-dark-laptop)
+(load-theme 'misterioso t)
 
 ;;; Set yaml mode with files ending in sls, for salt stack
 (add-to-list 'auto-mode-alist '("\\.sls\\'" . yaml-mode))
+(add-to-list 'auto-mode-alist '("\\.jinja\\'" . yaml-mode))
+
+;;; Add neo-tree function
+
+(add-to-list 'load-path "~/.emacs.d/personal/preload/neotree")
+(require 'neotree)
+(global-set-key [f8] 'neotree-toggle)
+
+(setq projectile-switch-project-action 'neotree-projectile-action)
+
+(setq magit-git-executable "/usr/local/git/bin/git")
 
 (provide 'prefs.el)
 ;;; prefs.el ends here
