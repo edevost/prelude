0d901fd594bb757d122018531f66472531ad517c
edevost <ericdevost@gmail.com>
2017-08-12 14:23:44 -0400

    Added org refile and agenda configuration

--- personal/preload/init.el
+++ personal/preload/init.el
@@ -5,3 +5,16 @@
 
 (package-initialize)
 (elpy-enable)
+;; Initialse org and setup capture
+(require 'org)
+(setq org-directory "~/GTD/")
+
+(setq org-refile-use-outline-path 'file)
+(setq org-default-notes-file (concat org-directory "intray.org"))
+(setq org-agenda-files '("~/GTD"))
+
+(define-key global-map "\C-cc" 'org-capture)
+(setq org-refile-targets '((org-agenda-files :maxlevel . 1)))
+
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
